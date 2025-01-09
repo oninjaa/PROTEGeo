e4ee994ee9d4a1b1db8da1667ab948d535405963
Lucas Leonardo <xmiiitz@gmail.com>
2025-01-09 17:12:59 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+import {nextui} from '@nextui-org/theme'
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+
+
+     
+    extend: {
+      screens: {
+        '2xl': '1440px',
+        '3xl': '2200px',
+      },
+      fontFamily: {
+        sans: ["var(--font-sans)"],
+        mono: ["var(--font-mono)"],
+      },
+    },
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+}
